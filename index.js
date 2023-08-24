85a1d8e6ba65a07e330aafe5e4e96c0f3d437458
Andy <andysoutcharith@gmail.com>
2023-08-23 23:52:19 -0500

    Added an event listenter to the search button that fetches the data from the movie api

---
+++ index.js
@@ -0,0 +1,11 @@
+const searchBtn = document.getElementById("search-btn");
+const searchInput = document.getElementById("search-input");
+
+const fetchMovies = function () {
+  fetch(`http://www.omdbapi.com/?apikey=aedabea0&t=${searchInput.value}`)
+    .then((response) => response.json())
+    .then((movies) => console.log(movies));
+  console.log(searchInput.value);
+};
+
+searchBtn.addEventListener("click", fetchMovies);
