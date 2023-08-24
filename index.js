1b6ecccb29f37ac9089a0a206cda275afe60a6ac
AndyS <andysoutcharith@gmail.com>
2023-08-24 01:11:49 -0500

    Added logic to see if a user leaves the search input blank and to log that it's empty

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const searchBtn = document.getElementById("search-btn");
 const searchInput = document.getElementById("search-input");
 
 const fetchMovies = function () {
-  fetch(`http://www.omdbapi.com/?apikey=aedabea0&t=${searchInput.value}`)
-    .then((response) => response.json())
-    .then((movies) => console.log(movies));
-  console.log(searchInput.value);
+  if (searchInput.value.trim() === "") {
+    console.log("Empty...");
+  } else {
+    fetch(`http://www.omdbapi.com/?apikey=aedabea0&t=${searchInput.value}`)
+      .then((response) => response.json())
+      .then((movies) => console.log(movies));
+    console.log(searchInput.value);
+  }
 };
 
 searchBtn.addEventListener("click", fetchMovies);
